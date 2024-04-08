22a544e4475954ffa82a02f9975cc53dc24fd470
An_Edric <ducan.co.6789@gmai.com>
2024-04-09 02:37:55 +0700

    test

--- server/database/conn.js
+++ server/database/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connect() {
   await mongoose.connect(
-    "mongodb+srv://ducanco6789:x5Rcqxo4XRBEPAN9@quiz.g06pvfg.mongodb.net/"
+    "mongodb+srv://ducanco6789:x5Rcqxo4XRBEPAN9@quiz.g06pvfg.mongodb.net/?retryWrites=true&w=majority"
   );
   console.log("Database Connected");
 }
